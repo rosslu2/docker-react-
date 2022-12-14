7fe979bdfdd2950efb938a33507574d31613d364
luka ross <rosslu@thehutgroup.com>
2022-12-14 18:10:59 +0000

    finished blog

--- Services/index.js
+++ Services/index.js
@@ -95,6 +95,43 @@ export const getCategories = async () => {
   return result.categories;
 };
 
+export const getCategoryPost = async (slug) => {
+  const query = gql`
+    query GetCategoryPost($slug: String!) {
+      postsConnection(where: { categories_some: { slug: $slug } }) {
+        edges {
+          cursor
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query, { slug });
+
+  return result.postsConnection.edges;
+};
+
 export const getPostDetails = async (slug) => {
   const query = gql`
     query GetPostDetails($slug: String!) {
--- components/index.js
+++ components/index.js
@@ -5,3 +5,4 @@ export { default as Header } from "./Header";
 export { default as Layout } from "./Layout";
 export { default as Author } from "./Author";
 export { default as PostDetail } from "./PostDetail";
+export { default as Loader } from "./Loader";
